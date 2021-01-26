e8911e8201fd709e75730fe1488c4041cc397ae9
GJFen <guo.jinfen@foxmail.com>
2021-01-27 00:03:17 +0800

    'V1.0.0'

---
+++ app1.js
@@ -0,0 +1,17 @@
+//http
+// var http = require('http');
+// http.createServer(function(req, res){
+//     res.writeHead(200);
+//     res.end('hello http server');
+// }).listen(3000);
+
+//express
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+    res.send('hello express');
+});
+app.all('/admin', function(req, res){
+    res.send('hello express all');
+});
+app.listen(3000);
